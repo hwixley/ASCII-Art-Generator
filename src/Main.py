b0dc535acbb01e1a2cdc6e11d66d02b63d47a2ec
Harry Wixley <hwixley1@gmail.com>
2023-05-17 23:55:16 +0100

    working

--- src/Main.py
+++ src/Main.py
@@ -2,6 +2,9 @@ import numpy as np
 from PIL import Image
 
 img = Image.open('../data/cage.jpg').convert('L')
+ratio = img.size[0] / img.size[1]
+new_width = 80
+img = img.resize((new_width, int(new_width / ratio)), Image.ANTIALIAS)
 
 img_arr = np.array(img)
 
@@ -21,6 +24,9 @@ ptg = 0.28
 img_arr[img_arr < ptg * 255] = 0
 
 img_arr[img_arr >= ptg * 255] = 255
+
+for row in img_arr:
+    print(",".join(map(str, row)).replace("0", "  ").replace("255", "XX").replace(",", ""))
 
 img2 = Image.fromarray(img_arr)
 
