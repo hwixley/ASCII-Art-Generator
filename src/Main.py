7a25aa6350629ca51500187aa8b966f489b146f1
Harry Wixley <hwixley1@gmail.com>
2023-05-18 02:28:54 +0100

    working :)

--- src/Main.py
+++ src/Main.py
@@ -1,33 +1,61 @@
 import numpy as np
 from PIL import Image
+import sys
+
+args = sys.argv
+
+def tuple_set(seq):
+    return {tuple(item) for item in seq}
 
 img = Image.open('../data/cage.jpg').convert('L')
+
 ratio = img.size[0] / img.size[1]
 new_width = 80
-img = img.resize((new_width, int(new_width / ratio)), Image.ANTIALIAS)
+img = img.resize((new_width, int(new_width / ratio)))
 
 img_arr = np.array(img)
-
 img.save('greyscale.png')
 
-# ptgs = [0, 0.1, 0.2, 0.3, 0.4, 0.5]
-ptg = 0.28
+bw = False
+ascii_print = True
 
-# for i, ptg in enumerate(ptgs[1:]):
-#     val = i+1/len(ptgs) * 255
-#     if i == len(ptgs) - 2:
-#         img_arr[img_arr >= ptg * 255] = val
-#     else:
-#         img_arr[img_arr < ptg * 255] = val
-#         img_arr[img_arr >= ptg * 255] = val
+if bw:
+    ptg = 0.28
+    img_arr[img_arr < ptg * 255] = 0
+    img_arr[img_arr >= ptg * 255] = 255
+else:
+    charset1 = "`.-':_,^=;><+!rc*/z?sLTv)J7(|Fi\{C\}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@"
+    charset2 = " .~+=@#"
+    charset3 = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{\}[]?-_+~<>i!lI;:,\"^`'."
+    charset4 = " .:-=+*#%@"
+    charset = charset4
+    ptg_types = ["linear", "log", "exp"]
+    ptg_type = ptg_types[0]
+    min_ptg = float(args[1]) if len(args) > 1 else 0.1
+    max_ptg = float(args[2]) if len(args) > 2 else 0.5
+    ptgs = np.empty(len(charset))
+    if ptg_type == "linear":
+        ptgs = np.linspace(min_ptg, max_ptg, len(charset)-1).tolist() + [1]
+    elif ptg_type == "log":
+        ptgs = np.logspace(min_ptg, np.log10(max_ptg), len(charset)-1).tolist() + [1]
+    elif ptg_type == "exp":
+        ptgs = np.exp(np.linspace(min_ptg, np.log(max_ptg), len(charset)-1)).tolist() + [1]
 
-img_arr[img_arr < ptg * 255] = 0
+    ascii_arr = [[" " for _ in range(img_arr.shape[1])] for _ in range(img_arr.shape[0])]
 
-img_arr[img_arr >= ptg * 255] = 255
+    for i, ptg in enumerate(ptgs):
+        last_ptg = ptgs[i-1] if i > 0 else 0
+        lt_idxs = tuple_set(np.argwhere(img_arr < ptg * 255).tolist()) # and img_arr >= last_ptg * 255)
+        gt_idxs = tuple_set(np.argwhere(img_arr >= last_ptg * 255).tolist())
 
-for row in img_arr:
-    print(",".join(map(str, row)).replace("0", "  ").replace("255", "XX").replace(",", ""))
+        idxs = list(set(lt_idxs).intersection(gt_idxs))
+
+        for idx in idxs:
+            ascii_arr[idx[0]][idx[1]] = charset[i]*2
+
+if ascii_print:
+    for row in ascii_arr:
+        print("".join(row))
 
 img2 = Image.fromarray(img_arr)
-
 img2.save('greyscale2.png')
