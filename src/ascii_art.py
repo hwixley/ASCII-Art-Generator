9cacd8b59f541c96835dc7f5fd729781275cd334
Harry Wixley <hwixley1@gmail.com>
2023-05-19 01:26:09 +0100

    optimized ascii_art functions

--- src/ascii_art.py
+++ src/ascii_art.py
@@ -13,28 +13,27 @@ class AsciiArt:
     def get_ascii_image(self):
         img_arr = self.args.get_resized_bw_img()
         charset = self.args.get_charset()
-        sampler = self.args.get_sampler()
-        lower_ptg = self.args.get_lower_ptg()
-        upper_ptg = self.args.get_upper_ptg()
 
-        ptgs = self.get_ptgs(len(charset), sampler, lower_ptg, upper_ptg)
-        return self.get_ascii_image_arr(img_arr, charset, ptgs)
-    
-    def get_ascii_image_arr(self, img_arr, charset, ptgs):
+        ptgs = self.get_ptgs(len(charset))
         ascii_arr = [[" " for _ in range(img_arr.shape[1])] for _ in range(img_arr.shape[0])]
 
         for i, ptg in enumerate(ptgs):
             last_ptg = ptgs[i-1] if i > 0 else 0
+
             lt_idxs = self.tuple_set(np.argwhere(img_arr < ptg * 255).tolist())
             gt_idxs = self.tuple_set(np.argwhere(img_arr >= last_ptg * 255).tolist())
 
             idxs = list(set(lt_idxs).intersection(gt_idxs))
-
             for idx in idxs:
                 ascii_arr[idx[0]][idx[1]] = charset[i]*2
+        
         return ascii_arr
 
-    def get_ptgs(self, n, sampler, lower_ptg, upper_ptg):
+    def get_ptgs(self, n):
+        sampler = self.args.get_sampler()
+        lower_ptg = self.args.get_lower_ptg()
+        upper_ptg = self.args.get_upper_ptg()
+
         if sampler == "linear":
             return [0] + np.linspace(lower_ptg, upper_ptg, n-2).tolist() + [1]
         elif sampler == "log":
